f53fcaa3054e4bd3c6b8a854d086380f19c68d41
RootZero <rootzero17@gmail.com>
2022-07-11 00:16:00 +0900

    Feat : 닉네임 localStorage에 저장

--- pages/signIn.tsx
+++ pages/signIn.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import reactLogo from "../public/main/reactLogo.png";
+import useRouteHandler from "hooks/useRouteHandler";
+import { Router } from "next/router";
 
 const signIn = () => {
+  const [nickname, setNickname] = useState("");
+
+  const route = useRouteHandler();
+  const onClick = () => {
+    localStorage.setItem("nickname", nickname);
+    route("/gameList");
+  };
+
   return (
     <div className="wrapper border-blue">
       <Image src={reactLogo} />
       <h2>사용하실 닉네임을 입력해주세요.</h2>
       <h3 className="mt-2">(2~6글자로 입력해주세요.)</h3>
-      <form className="w-full">
+      <form className="w-full" onSubmit={onClick}>
         <div className="w-full space-x-20 flex items-center justify-center mt-10">
           <input
             pattern=".{2,6}"
             className=" w-1/2 bg-black border-10 border-white text-white p-10 text-60 text-center"
+            onChange={(event) => setNickname(event?.target.value)}
             required
           />
           <button
