dab9abadcde29c7dd02d198369ed28aaf5b3b155
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-10 09:50:25 -0400

    feat: Star Component, GameList Page View

--- pages/gameList.tsx
+++ pages/gameList.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import Stars from "components/velaga/Stars";
 
 const gameList = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen bg-black">
-      <h1>게임</h1>
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-black relative">
+      <Stars />
+      <span className="text-100 text-white">게임</span>
+      <div className="flex flex-col items-center justify-center w-[1000px] h-[150px] text-white border-white border-4 mt-10 hover:bg-white hover:text-black ">
+        <span className="text-50">V E L A G A</span>
+      </div>
+      <div className="flex flex-col items-center justify-center w-[1000px] h-[150px] text-white border-white border-4 mt-10 hover:bg-white hover:text-black ">
+        <span className="text-50">지금 준비중 입니다...</span>
+      </div>
+      <div className="flex flex-col items-center justify-center w-[1000px] h-[150px] text-white border-white border-4 mt-10 hover:bg-white hover:text-black ">
+        <span className="text-50">지금 준비중 입니다...</span>
+      </div>
     </div>
   );
 };
